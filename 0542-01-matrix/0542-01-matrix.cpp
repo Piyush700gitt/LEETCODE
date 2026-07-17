dff2ce268159db2c1ba3e994f7deda4bdefa5f5d
Piyush700gitt <shrivastavapiyush700@gmail.com>
2026-07-17 16:13:11 +0530

    Time: 30 ms (20.44%), Space: 38.1 MB (43.67%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        vector<vector<int>>distance(n,vector<int>(m,-1));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    visited[i][j]=true;
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+            q.pop();
+            distance[row][col]=steps;
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol]){
+                    q.push({{nrow,ncol},steps+1});
+                    visited[nrow][ncol]=true;
+                }
+            }
+        }
+        return distance;
+        
+    }
+};
